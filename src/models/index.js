e3597656488537b1d3a54e6af8cbfce716014e90
Eric <eheitmuller@gmail.com>
2020-08-01 18:40:43 -0700

    Changes to run on heroku for DB url/seeding

--- src/models/index.js
+++ src/models/index.js
@@ -3,15 +3,24 @@ import { Sequelize, DataTypes } from 'sequelize';
 import message from './message'
 import user from './user'
 
-
-const sequelize = new Sequelize( 
-  process.env.TEST_DATABASE || process.env.DATABASE,
-  process.env.DATABASE_USER, 
-  process.env.DATABASE_PASSWORD, 
-  {
-    dialect: 'postgres', 
-  },
-);
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(
+    process.env.DATABASE_URL, 
+    { 
+      dialect: 'postgres',    
+    }
+  );
+} else {
+  sequelize = new Sequelize( 
+    process.env.TEST_DATABASE || process.env.DATABASE,
+    process.env.DATABASE_USER, 
+    process.env.DATABASE_PASSWORD, 
+    {
+      dialect: 'postgres', 
+    },
+  );
+}
 
 const models = {
   User: user(sequelize, DataTypes), 
